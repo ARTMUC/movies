c4835481818f13cec490b176b90db2a299b3e411
ARTMUC <89223118+ARTMUC@users.noreply.github.com>
2022-05-08 22:19:33 +0200

    feat: setup docs, cookie-parser, cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { PrismaService } from 'prisma/prisma.service';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Movies API')
+    .setDescription('Movies API docs')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  app.use(cookieParser());
+  app.enableCors({
+    credentials: true,
+  });
+
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app)
   await app.listen(3001);
